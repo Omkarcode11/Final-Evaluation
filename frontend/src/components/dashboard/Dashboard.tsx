65059f929c7207c548395630731e6ec4023c9504
omkarcode11 <omkarsonawaneomkar2@gmail.com>
2024-08-19 17:55:08 +0530

    dashboard ui complete done

--- frontend/src/components/dashboard/Dashboard.tsx
+++ frontend/src/components/dashboard/Dashboard.tsx
@@ -1,6 +1,46 @@
+import QuizCard from "../cards/quizCard/QuizCard"
+import TrendingQuizeCard from "../cards/trendingQuizCards/TrendingQuizeCard"
+import classes from './Dashboard.module.css'
+
+let titles = [
+    {number:12,title:'Quiz',color:"#FF5D01"},
+    {number:110,title:'Questions',color:"#60B84B"},
+    {number:'1.4k',title:'Total',color:"#5076FF"},
+]
+
+let quizes = [
+  {name:"Quiz1",views:484,createdAt:'04 Aug 2024'},
+  {name:"Quiz1",views:484,createdAt:'04 Aug 2024'},
+  {name:"Quiz1",views:484,createdAt:'04 Aug 2024'},
+  {name:"Quiz1",views:484,createdAt:'04 Aug 2024'},
+  {name:"Quiz1",views:484,createdAt:'04 Aug 2024'},
+  {name:"Quiz1",views:484,createdAt:'04 Aug 2024'},
+  {name:"Quiz1",views:484,createdAt:'04 Aug 2024'},
+  {name:"Quiz1",views:484,createdAt:'04 Aug 2024'},
+  {name:"Quiz1",views:484,createdAt:'04 Aug 2024'},
+  {name:"Quiz1",views:484,createdAt:'04 Aug 2024'},
+  {name:"Quiz1",views:484,createdAt:'04 Aug 2024'},
+  {name:"Quiz1",views:484,createdAt:'04 Aug 2024'},
+]
+
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div className={classes.container}>
+    <div className={classes.cardContainer}>
+        {titles.map(ele=>
+        <TrendingQuizeCard number={ele.number} title={ele.title} color={ele.color}/>
+    )}
+    </div>
+    <div className={classes.quizCardContainer}>
+      <h1>Trending Quizs</h1>
+      <div className={classes.quizContainer}>
+        {quizes.map(ele=>
+        <QuizCard views={ele.views} createdAt={ele.createdAt} name={ele.name}/>
+        )}
+      </div>
+    </div>
+
+    </div>
   )
 }
 
