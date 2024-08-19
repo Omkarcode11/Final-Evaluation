be37504241d5885a40b6f42ba44d54c054df2919
omkarcode11 <omkarsonawaneomkar2@gmail.com>
2024-08-19 17:54:25 +0530

    analytics ui complete

--- frontend/src/components/analytics/Analytics.tsx
+++ frontend/src/components/analytics/Analytics.tsx
@@ -1,7 +1,18 @@
-function Analytics() {
-  return (
-    <div>Analytics</div>
-  )
+import classes from "./Analytics.module.css";
+import QuizTable from "../quizTable/table/QuizTable";
+
+
+type Props = {};
+
+function Analytics({}: Props) {
+  return( 
+  <div className={classes.container}>
+    <h1 className={classes.header}>Quiz Analysis</h1>
+    <div className={classes.quizTableContainer}>
+     <QuizTable/>
+    </div>
+    </div>
+    );
 }
 
-export default Analytics
+export default Analytics;
