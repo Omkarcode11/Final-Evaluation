77a581e97710c62bbab34e67060cc5e4b76678fd
omkarcode11 <omkarsonawaneomkar2@gmail.com>
2024-08-22 04:46:42 +0530

    define the user route with getstats and getTrending quiz

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -3,25 +3,44 @@ const User = require("../models/User");
 exports.getMyQuizStats = async (req, res) => {
   try {
     let id = req.user._id;
-    let userInfo = await User.findById(id).populate("quizzes").populate('questions')
+    let userInfo = await User.findById(id)
+      .populate("quizzes")
+      .populate("questions");
 
-    let totalQuestions = 0 
-    let totalImpression = 0
-    for(let ques of userInfo.quizzes){
-        totalQuestions += ques.questions.length
-        totalImpression += ques.impression
+    let totalQuestions = 0;
+    let totalImpression = 0;
+    for (let ques of userInfo.quizzes) {
+      totalQuestions += ques.questions.length;
+      totalImpression += ques.impression;
     }
 
     let data = {
-        quizCreated : userInfo.quizzes.length,
-        totalQuestions,
-        totalImpression
-    }
+      quizCreated: userInfo.quizzes.length,
+      totalQuestions,
+      totalImpression,
+    };
 
-
-    return res.status(200).json({data});
+    return res.status(200).json({ data });
   } catch (err) {
     res.status(500).send(err);
     console.log(err.message);
+  }
+};
+
+exports.getTrendingQuiz = async (req, res) => {
+  try {
+    let id = req.user._id;
+    let quizzes = await User.findById(id)
+      .populate({
+        path: "quizzes",
+        match: { impression: { $gt: 10 } }, // Filters quizzes with impression greater than 10
+        options: { sort: { createdAt: 1 } }, // Sorts by createdAt in ascending order
+      })
+      .select("quizzes");
+
+    return res.status(200).json(quizzes);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send(err.message);
   }
 };
