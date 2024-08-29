36a4321fc0da8e22ed90f493d9641261169c2781
omkarcode11 <omkarsonawaneomkar2@gmail.com>
2024-08-29 08:29:32 +0530

    add .env file

--- frontend/src/utils/constant.ts
+++ frontend/src/utils/constant.ts
@@ -1,3 +1,2 @@
-export const BASE_URL = 'http://localhost:8000'
-
-export const CURRENT_URL = 'http://localhost:5173'
+export const BASE_URL = import.meta.env.VITE_API_URL
+export const CURRENT_URL = import.meta.env.VITE_CURRENT_URL
